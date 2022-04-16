3bd1fbf6be369ed45e2bafff9bb07562d07cb8d0
Julien Berndt <julien.berndt@student.isae-supaero.fr>
2022-04-16 16:31:55 +0200

    Update tests and views for hidden results urls

--- mysite/polls/tests.py
+++ mysite/polls/tests.py
@@ -115,3 +115,24 @@ class QuestionDetailViewTests(TestCase):
         url = reverse('polls:detail', args=(past_question.id,))
         response = self.client.get(url)
         self.assertContains(response, past_question.question_text)
+        
+class QuestionResultsViewTests(TestCase):
+    def test_future_results(self):
+        """
+        The results view of a question with a pub_date in the future
+        returns a 404 not found.
+        """
+        future_question = create_question(question_text='Future question.', days=5)
+        url = reverse('polls:results', args=(future_question.id,))
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 404)
+
+    def test_past_results(self):
+        """
+        The results view of a question with a pub_date in the past
+        displays the results text.
+        """
+        past_question = create_question(question_text='Past question.', days=-5)
+        url = reverse('polls:results', args=(past_question.id,))
+        response = self.client.get(url)
+        self.assertContains(response, past_question.question_text)
