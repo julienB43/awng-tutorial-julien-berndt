b03ae99313a0e0e319a2bb411df3e711be3a0251
Julien Berndt <julien.berndt@student.isae-supaero.fr>
2022-04-16 20:50:57 +0200

    Update admin page

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -3,5 +3,26 @@ from django.contrib import admin
 from .models import Question
 from .models import Choice
 
-admin.site.register(Question)
-admin.site.register(Choice)
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 0
+    
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,               {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+    ]
+    inlines = [ChoiceInline]
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+    list_filter = ['pub_date']
+    
+class ChoiceAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ('Paramètre choix',               {'fields': ['question', 'choice_text']}),
+        ('Trucage', {'fields': ['votes']}),
+    ]
+    list_display = ('choice_text', 'question', 'votes')
+
+admin.site.register(Question,QuestionAdmin)
+admin.site.register(Choice,ChoiceAdmin)
--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,5 +1,5 @@
 import datetime
-
+from django.contrib import admin
 from django.db import models
 from django.utils import timezone
 
@@ -7,8 +7,16 @@ from django.utils import timezone
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
+    
     def __str__(self):
         return self.question_text
+    
+    @admin.display(
+        boolean=True,
+        ordering='pub_date',
+        description='Published recently?',
+    )
+    
     def was_published_recently(self):
         now = timezone.now()
         return now - datetime.timedelta(days=1) <= self.pub_date <= now
@@ -18,5 +26,10 @@ class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
+    
+    @admin.display(
+        ordering='votes',
+    )
+    
     def __str__(self):
         return self.choice_text
